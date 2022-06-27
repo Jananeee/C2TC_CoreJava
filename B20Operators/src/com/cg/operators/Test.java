e4970ffdd593056c57f0fc7d324b0f166da6051d
HP <HP@LAPTOP-1LFAAOT5>
2022-06-27 13:25:18 +0530

    first commit

---
+++ B20Operators/src/com/cg/operators/Test.java
@@ -0,0 +1,12 @@
+package com.cg.operators;
+
+public class Test {
+
+	public static void main(String[] args) {
+		float f= (float) ((0.25)*10); // decimals
+		System.out.println(f);
+		int i= (int)f; //typecasting -->0.0-->0
+		System.out.println(i); //0
+	}
+
+}
