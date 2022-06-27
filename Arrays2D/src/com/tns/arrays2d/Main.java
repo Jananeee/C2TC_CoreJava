1c27149988e346c7e98628d76e2d41ad6c1e68d2
HP <HP@LAPTOP-1LFAAOT5>
2022-06-27 12:47:00 +0530

    first commit

---
+++ Arrays2D/src/com/tns/arrays2d/Main.java
@@ -0,0 +1,19 @@
+package com.tns.arrays2d;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int array[][]= {{42,2,41},{52,1,40},{67,9,12}};
+		shuffle(array);
+	}  //end of the main method
+	public static void shuffle(int array[][]) {
+		for(int i=0;i<3;i++) {
+			for(int j=0;j<3;j++) {
+				System.out.print(array[i][j]+ " ");
+			}          //end of inner loop
+			System.out.println();
+		}         //end of outer for loop
+		
+	}
+
+}
